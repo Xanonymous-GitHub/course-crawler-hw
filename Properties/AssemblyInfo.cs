31799c9163bf47b5c6a4734895ffffaad119f305
Xanonymous <trusaidlin@gmail.com>
2021-10-02 00:38:48 +0800

    add properties

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("CourseCrawler")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyCompany("Xanonymous")]
 [assembly: AssemblyProduct("CourseCrawler")]
-[assembly: AssemblyCopyright("Copyright © HP 2021")]
+[assembly: AssemblyCopyright("Copyright © Xanonymous 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
