f2d0d000b899c9ec2fd9ff5384a3f00d373b07b9
Xanonymous <trusaidlin@gmail.com>
2021-10-03 00:26:08 +0800

    add crawler usecase

---
+++ CrawlerUseCase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using HtmlAgilityPack;
+
+namespace CourseCrawler
+{
+    internal interface ICrawlerUseCase
+    {
+        HtmlDocument Do();
+    }
+
+    internal class CrawlerUseCase : ICrawlerUseCase
+    {
+        public CrawlerUseCase(Uri uri)
+        {
+            _uri = uri;
+            _webClient = new HtmlWeb();
+            _webClient.OverrideEncoding = Encoding.Default;
+        }
+
+        private readonly Uri _uri;
+        private readonly HtmlWeb _webClient;
+
+        public HtmlDocument Do()
+        {
+            // TODO Add StatusCode Check & Error handlers.
+            // TODO Make request process async.
+            return _webClient.Load(_uri);
+        }
+    }
+}
