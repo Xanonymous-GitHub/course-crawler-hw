023ea27680e71c4cfe442aa1d639a2c57bab6478
Xanonymous <trusaidlin@gmail.com>
2021-11-13 06:17:48 +0800

    add ExpectNotified method

--- CourseCrawlerTest/src/Pool.cs
+++ CourseCrawlerTest/src/Pool.cs
@@ -1,13 +1,36 @@
-﻿using System;
+﻿using System.ComponentModel;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
 
-namespace CourseCrawlerTest
+namespace CourseCrawler.Tests
 {
-    internal static class Pool
+    public static class Pool
     {
+        // ExpectNotified
+        public static void ExpectNotified<T>(INotifyPropertyChanged observable, string propertyName, T newValue)
+        {
+            Assert.IsNotNull(observable);
 
+            bool notified = false;
+            observable.PropertyChanged += delegate (object sender, PropertyChangedEventArgs e)
+            {
+                notified = e.PropertyName == propertyName;
+            };
+
+            System.Type observableRunTimeType = observable.GetType();
+            Assert.IsNotNull(observableRunTimeType);
+            Assert.IsInstanceOfType(observable, observableRunTimeType);
+
+            PropertyInfo propertyInfo = observableRunTimeType.GetProperty(propertyName);
+            Assert.IsNotNull(propertyInfo);
+            Assert.AreNotEqual(newValue, propertyInfo.GetValue(observable, null));
+
+            propertyInfo.SetValue(observable, newValue, null);
+            Assert.IsTrue(notified);
+        }
     }
 }
