18a86061c2acd69f8d966948c9cd566a67629496
Xanonymous <trusaidlin@gmail.com>
2021-11-13 06:18:22 +0800

    finish startUpFormVM tests

--- CourseCrawlerTest/src/ViewModel/StartUpFormViewModelTests.cs
+++ CourseCrawlerTest/src/ViewModel/StartUpFormViewModelTests.cs
@@ -1,28 +1,44 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CourseCrawler;
-using System;
+using System.ComponentModel;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace CourseCrawler.Tests
 {
     [TestClass()]
     public class StartUpFormViewModelTests
     {
+        private StartUpFormViewModel _startUpFormViewModel;
+
+        [TestInitialize()]
+        public void Init()
+        {
+            _startUpFormViewModel = new();
+        }
+
         // CorrectlyCreateStartUpFormViewModel
         [TestMethod()]
         public void CorrectlyCreateStartUpFormViewModel()
         {
-            // Given
-            StartUpFormViewModel startUpFormViewModel;
+            Assert.IsNotNull(_startUpFormViewModel);
+        }
 
-            // When
-            startUpFormViewModel = new();
+        // ExpectAcceptToOpenSelectCourseFormChangeNotified
+        [TestMethod()]
+        public void ExpectAcceptToOpenSelectCourseFormChangeNotified()
+        {
+            Pool.ExpectNotified(_startUpFormViewModel, nameof(_startUpFormViewModel.AcceptToOpenSelectCourseForm), false);
+        }
 
-            // Then
-            Assert.IsNotNull(startUpFormViewModel);
+        // ExpectAcceptToOpenCourseManagementFormChangeNotified
+        [TestMethod()]
+        public void ExpectAcceptToOpenCourseManagementFormChangeNotified()
+        {
+            Pool.ExpectNotified(_startUpFormViewModel, nameof(_startUpFormViewModel.AcceptToOpenCourseManagementForm), false);
         }
     }
 }
