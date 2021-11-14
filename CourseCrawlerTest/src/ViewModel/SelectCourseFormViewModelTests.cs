98b994f5638d7baf0ab672f9fe1fff7466dffe20
Xanonymous <trusaidlin@gmail.com>
2021-11-14 16:46:32 +0800

    finish selectCourseFormViewModel Tests

--- CourseCrawlerTest/src/ViewModel/SelectCourseFormViewModelTests.cs
+++ CourseCrawlerTest/src/ViewModel/SelectCourseFormViewModelTests.cs
@@ -11,18 +11,68 @@ namespace CourseCrawler.Tests
     [TestClass()]
     public class SelectCourseFormViewModelTests
     {
+        private SelectCourseFormViewModel _selectCourseFormViewModel;
+        const int testDataSourceIndex = 0;
+        const int testDisplayTabIndex = 0;
+
+        [TestInitialize()]
+        public void Init()
+        {
+            _selectCourseFormViewModel = new();
+        }
+
         // CorrectlyCreateSelectCourseFormViewModel
         [TestMethod()]
         public void CorrectlyCreateSelectCourseFormViewModel()
         {
-            // Given
-            SelectCourseFormViewModel selectCourseFormViewModel;
+            Assert.IsNotNull(_selectCourseFormViewModel);
+        }
 
-            // When
-            selectCourseFormViewModel = new();
+        // CorrectlyGetCourseTableRows
+        [TestMethod()]
+        public void CorrectlyGetCourseTableRows()
+        {
+            List<string[]> rows = _selectCourseFormViewModel.GetCourseTableRows(testDataSourceIndex);
+            Assert.IsNotNull(rows);
+        }
 
-            // Then
-            Assert.IsNotNull(selectCourseFormViewModel);
+        [TestMethod()]
+        // CorrectlyChangeCourseCheckStatus
+        public void CorrectlyCheckACourse()
+        {
+            _selectCourseFormViewModel.ChangeCourseCheckStatus(testDisplayTabIndex, true, testDataSourceIndex);
+            Assert.IsTrue(_selectCourseFormViewModel.IsAnyCourseChecked());
+
+            _selectCourseFormViewModel.MakeAllUnCheck();
+            Assert.IsFalse(_selectCourseFormViewModel.IsAnyCourseChecked());
+        }
+
+        // SubmmitCourseSelectionRequestWithoutConflicts
+        [TestMethod()]
+        public void SubmmitCourseSelectionRequestWithoutConflicts()
+        {
+            _selectCourseFormViewModel.MakeAllUnCheck();
+            _selectCourseFormViewModel.ChangeCourseCheckStatus(testDisplayTabIndex, true, testDataSourceIndex);
+            Assert.IsFalse(_selectCourseFormViewModel.IsAnyCourseSelected());
+            Result submissionResult = _selectCourseFormViewModel.HandleSelectCourseSubmission();
+            Assert.IsTrue(submissionResult.Success);
+        }
+
+        // SubmmitCourseSelectionRequestWithConflicts
+        [TestMethod()]
+        public void SubmmitCourseSelectionRequestWithConflicts()
+        {
+            _selectCourseFormViewModel.MakeAllUnCheck();
+
+            int courseAmountInTable = _selectCourseFormViewModel.GetCourseTableRows(testDataSourceIndex).Count;
+
+            for (int i = 0; i < courseAmountInTable; i++)
+            {
+                _selectCourseFormViewModel.ChangeCourseCheckStatus(i, true, testDataSourceIndex);
+            }
+            
+            Result submissionResult = _selectCourseFormViewModel.HandleSelectCourseSubmission();
+            Assert.IsTrue(submissionResult.Failure);
         }
     }
 }
